ded88d2a115a171f757460523d44736f230282a9
Matthew Toghill <matthew.toghill@outlook.com>
2025-12-01 22:26:57 +0000

    feat: update use of nameof with support for unbound generic types

--- src/Content/Backend/Solution/Monaco.Template.Backend.Common.Application/Queries/QueryPagedBase.cs
+++ src/Content/Backend/Solution/Monaco.Template.Backend.Common.Application/Queries/QueryPagedBase.cs
@@ -5,14 +5,14 @@ namespace Monaco.Template.Backend.Common.Application.Queries;
 
 public abstract record QueryPagedBase<T>(IEnumerable<KeyValuePair<string, StringValues>> QueryParams) : QueryBase<Page<T>?>(QueryParams)
 {
-	public virtual int Offset => QueryParams.FirstOrDefault(x => x.Key.Equals(nameof(Page<T>.Pager.Offset), StringComparison.InvariantCultureIgnoreCase))
+	public virtual int Offset => QueryParams.FirstOrDefault(x => x.Key.Equals(nameof(Page<>.Pager.Offset), StringComparison.InvariantCultureIgnoreCase))
 											.Value
 											.Select(x => int.TryParse(x, out var y) ? y : 0)
 											.Where(x => x >= 0)
 											.DefaultIfEmpty(0)
 											.FirstOrDefault();
 
-	public virtual int Limit => QueryParams.FirstOrDefault(x => x.Key.Equals(nameof(Page<T>.Pager.Limit), StringComparison.InvariantCultureIgnoreCase))
+	public virtual int Limit => QueryParams.FirstOrDefault(x => x.Key.Equals(nameof(Page<>.Pager.Limit), StringComparison.InvariantCultureIgnoreCase))
 										   .Value
 										   .Select(x => int.TryParse(x, out var y) ? y : 0)
 										   .Where(x => x is > 0 and <= 100)
